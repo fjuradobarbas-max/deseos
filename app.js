1ebc1b11906bfee3f570b44dd82b43df77097c1f
fernando-jurado <fernando.jurado@vodafone.com>
2026-09-08 15:45:15 +0200

    reinicio

---
+++ app.js
@@ -0,0 +1,41 @@
+let selectedUser = '';
+
+const homeScreen = document.getElementById('home-screen');
+const editorScreen = document.getElementById('editor-screen');
+
+const selectedUserText =
+    document.getElementById('selected-user');
+
+document
+    .getElementById('fernando-btn')
+    .addEventListener('click', () => {
+
+        selectedUser = 'Fernando';
+
+        selectedUserText.textContent =
+            selectedUser;
+
+        homeScreen.classList.add('hidden');
+        editorScreen.classList.remove('hidden');
+    });
+
+document
+    .getElementById('debora-btn')
+    .addEventListener('click', () => {
+
+        selectedUser = 'Debora';
+
+        selectedUserText.textContent =
+            selectedUser;
+
+        homeScreen.classList.add('hidden');
+        editorScreen.classList.remove('hidden');
+    });
+
+document
+    .getElementById('back-btn')
+    .addEventListener('click', () => {
+
+        editorScreen.classList.add('hidden');
+        homeScreen.classList.remove('hidden');
+    });
